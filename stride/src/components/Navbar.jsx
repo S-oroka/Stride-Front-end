b00e6f506c6a029fee1a98e0699aab62f016917a
Jakub <sorok028@umn.edu>
2023-03-28 14:58:19 -0500

    created conditional for signed in or not

--- stride/src/components/Navbar.jsx
+++ stride/src/components/Navbar.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 
-const Navbar = () => {
+const Navbar = ({user, handleLogout}) => {
+  let signedIn
+       if (user) {
+         signedIn = 
+         <header>
+           <h1>{user.name}</h1>
+           <button onClick={handleLogout}>Logout</button>
+         </header>
+       }
+       let signedOut = (
+         <header
+         ></header>
+       )
   return (
     <div>
-      <>
-        <h1 className='text-blue-500'>This is the navbar</h1>
-      </>
+      {user ? signedIn : signedOut}
     </div>
   )
 }
