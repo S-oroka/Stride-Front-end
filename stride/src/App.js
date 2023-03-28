eeebe4ff75584ffc6969527d9c8fed8875a4a006
Jakub <sorok028@umn.edu>
2023-03-28 14:50:03 -0500

    created authentication

--- stride/src/App.js
+++ stride/src/App.js
@@ -1,16 +1,39 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { CheckSession } from './services/Auth';
+import { Route, Routes, useNavigate } from 'react-router-dom'
+
 
 function App() {
 
   const [user, setUser] = useState(null);
 
+  let navigate = useNavigate();
+
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+  }
+
+  const handleLogout = () => {
+    setUser(null)
+    localStorage.clear()
+    navigate('/')
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
   return (
     <div className="App">
       <>
-        <Navbar />
+        <Navbar user={user}/>
       </>
       <>
         <HomePage />
