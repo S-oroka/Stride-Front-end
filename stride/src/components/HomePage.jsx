3fc39ba25bf50cfde3a51513ab9461ce35178d3c
Jakub <sorok028@umn.edu>
2023-03-29 20:49:50 -0500

    mobile friendly navbar, homepage show runs, create and delete runs

--- stride/src/components/HomePage.jsx
+++ stride/src/components/HomePage.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { FaPlusCircle } from 'react-icons/fa';
 import axios from 'axios'
 
 const HomePage = ({ user }) => {
   const { id } = user ? user : {};
 
   const [runs, setRuns] = useState(null)
+  const [showForm, setShowForm] = useState(false)
+  const [distance, setDistance] = useState('')
+  const [time, setTime] = useState('')
 
   const getUserRuns = async () => {
     try {
@@ -18,6 +22,25 @@ const HomePage = ({ user }) => {
     }
   }
 
+  const deleteRun = async (id) => {
+    await axios.delete(`http://localhost:3001/api/runs/${id}`)
+    getUserRuns()
+  }
+  
+
+  const createRun = async () => {
+    const newRun = { distance, time }
+    const run = await axios.post(`http://localhost:3001/api/runs/${id}`, newRun)
+    setShowForm(false)
+    setDistance('')
+    setTime('')
+    getUserRuns()
+  }
+
+useEffect(() => {
+    getUserRuns()
+  }, [])
+
   useEffect(() => {
     if (user && id) { // added id check
       getUserRuns()
@@ -25,28 +48,32 @@ const HomePage = ({ user }) => {
   }, [user, id]) // added id dependency
 
   return (
-    <div className='mt-10 max-w-screen flex justify-center'>
-        <div>
-          <h1>Your Runs</h1>
-          {user && runs && runs.length > 0 ? (
-            <ul>
-              {runs.map(run => (
-                <li key={run.id} className="my-5">
-                  <h1>Distance: {run.distance}</h1>
-                   <h2>Time: {run.time}</h2>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>No runs found.</p>
-          )}
+    <div className='mt-10 max-w-screen-md mx-auto flex flex-col justify-center items-center'>
+      <h1 className='text-3xl mb-4'>Your Runs</h1>
+      {!showForm && <FaPlusCircle onClick={() => setShowForm(true)} size={30} className='text-4xl text-red-400 hover:cursor-pointer' />}
+      {showForm && (
+        <div className='mb-4'>
+          <input type='number' placeholder='Miles' value={distance} onChange={(e) => setDistance(e.target.value)} />
+          <input type='text' placeholder='hr:mn:sec' value={time} onChange={(e) => setTime(e.target.value)} />
+          <button onClick={createRun}>Submit</button>
+          <button onClick={() => setShowForm(false)}>Cancel</button>
         </div>
-        <div>
-          
-        </div>
+      )}
+      {user && runs && runs.length > 0 ? (
+        <ul className='w-full'>
+          {runs.map(run => (
+            <li key={run.id} className="my-5 border-b pb-3">
+              <h1 className='text-xl'>Distance: {run.distance} mi.</h1>
+              <h2 className='text-md'>Time: {run.time}</h2>
+              <button className='text-sm text-blue-400' onClick={() => deleteRun(run.id)}>X</button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className='text-lg text-center'>Please Sign In</p>
+      )}
     </div>
   )
 }
 
 export default HomePage
-
--- stride/src/components/Navbar.jsx
+++ stride/src/components/Navbar.jsx
@@ -1,50 +1,130 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { useState } from 'react'
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = ({ user, handleLogout }) => {
-  const [nav, setNav] = useState(false)
-  let signedIn
-  if (user) {
-    signedIn =
-      <header>
-        <div className='text-2xl ml-5 text-white'>
-          <NavLink to='/'>Stride</NavLink>
-        </div>
-        <ul className='px-5 flex items-center flex-row'>
+  const [nav, setNav] = useState(false);
 
-          <li className='px-4 cursor-pointer capitalize font-medium text-white hover:scale-105 duration-150'>
-            <NavLink onClick={handleLogout} to='/'>Sign Out</NavLink>
-
-          </li>
-
-          <li className='px-4 cursor-pointer capitalize font-medium text-white hover:scale-105 duration-150'>
-            <NavLink to='/about'>About</NavLink>
-          </li>
-        </ul>
-      </header>
-  }
-  let signedOut = (
-    <header className='flex justify-between items-center text-white w-full z-10'>
-      <div className='text-2xl ml-5 text-white'>
-        <NavLink to='/'>Stride</NavLink>
-      </div>
-      <ul className='px-5 items-center flex flex-row'>
-        <li className='px-4 cursor-pointer capitalize font-medium text-white hover:scale-105 duration-150'>
-          <NavLink to='/login'>Sign In</NavLink>
-        </li>
-        <li className='px-4 cursor-pointer capitalize font-medium text-white hover:scale-105 duration-150'>
-          <NavLink to='/about'>About</NavLink>
-        </li>
-      </ul>
-    </header>
-  )
   return (
-    <div className='flex justify-between items-center w-full h-20 px-4 bg-slate-700'>
-      {user ? signedIn : signedOut}
+    <div className="flex justify-between items-center w-full h-20 px-4 bg-slate-700">
+      <div className="text-2xl ml-5 text-white">Stride</div>
+
+      {user ? (
+        <>
+          <ul className="hidden md:flex items-center">
+            <li className="px-4 cursor-pointer capitalize font-medium text-white hover:scale-105 duration-150">
+              About
+            </li>
+            <li
+              className="px-4 cursor-pointer capitalize font-medium text-white hover:scale-105 duration-150"
+              onClick={handleLogout}
+            >
+              Sign Out
+            </li>
+          </ul>
+
+          <div
+            onClick={() => setNav(!nav)}
+            className="cursor-pointer pr-4 z-10 text-white md:hidden"
+          >
+            {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+          </div>
+
+          {nav && (
+            <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-blugraye-500 text-gray-500">
+              <li
+                className="px4 cursor-pointer capitalize py-6 text-4xl"
+                onClick={() => setNav(false)}
+              >
+                About
+              </li>
+              <li
+                className="px4 cursor-pointer capitalize py-6 text-4xl"
+                onClick={handleLogout}
+              >
+                Sign Out
+              </li>
+            </ul>
+          )}
+        </>
+      ) : (
+        <>
+          <ul className="hidden md:flex items-center">
+            <li className="px-4 cursor-pointer capitalize font-medium text-white hover:scale-105 duration-150">
+              About
+            </li>
+            <li className="px-4 cursor-pointer capitalize font-medium text-white hover:scale-105 duration-150">
+              Sign In
+            </li>
+          </ul>
+
+          <div
+            onClick={() => setNav(!nav)}
+            className="cursor-pointer pr-4 z-10 text-white md:hidden"
+          >
+            {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+          </div>
+
+          {nav && (
+            <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-600 text-gray-500">
+              <li
+                className="px4 cursor-pointer capitalize py-6 text-4xl"
+                onClick={() => setNav(false)}
+              >
+                About
+              </li>
+              <NavLink to='/login' className="px4 cursor-pointer capitalize py-6 text-4xl">Sign In</NavLink>
+            </ul>
+          )}
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
+
+export default Navbar;
 
 
-export default Navbar
+// const Navbar = ({ user, handleLogout }) => {
+//   const [nav, setNav] = useState(false)
+//   let signedIn
+//   if (user) {
+//     signedIn =
+//       <header>
+//         <div className='text-2xl ml-5 text-white'>
+//           <NavLink to='/'>Stride</NavLink>
+//         </div>
+//         <ul className='px-5 flex items-center flex-row'>
+
+//           <li className='px-4 cursor-pointer capitalize font-medium text-white hover:scale-105 duration-150'>
+//             <NavLink onClick={handleLogout} to='/'>Sign Out</NavLink>
+
+//           </li>
+
+//           <li className='px-4 cursor-pointer capitalize font-medium text-white hover:scale-105 duration-150'>
+//             <NavLink to='/about'>About</NavLink>
+//           </li>
+//         </ul>
+//       </header>
+//   }
+//   let signedOut = (
+//     <header className='flex justify-between items-center text-white w-full z-10'>
+//       <div className='text-2xl ml-5 text-white'>
+//         <NavLink to='/'>Stride</NavLink>
+//       </div>
+//       <ul className='px-5 items-center flex flex-row'>
+//         <li className='px-4 cursor-pointer capitalize font-medium text-white hover:scale-105 duration-150'>
+//           <NavLink to='/login'>Sign In</NavLink>
+//         </li>
+//         <li className='px-4 cursor-pointer capitalize font-medium text-white hover:scale-105 duration-150'>
+//           <NavLink to='/about'>About</NavLink>
+//         </li>
+//       </ul>
+//     </header>
+//   )
+//   return (
+//     <div className='flex justify-between items-center w-full h-20 px-4 bg-slate-700'>
+//       {user ? signedIn : signedOut}
+//     </div>
+//   )
+// }
