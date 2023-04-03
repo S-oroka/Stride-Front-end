040df35f7a93298c02d9ff23770d0ece421b9d43
Jakub <sorok028@umn.edu>
2023-04-02 22:14:31 -0500

    edit run working

--- stride/src/components/RunDetails.jsx
+++ stride/src/components/RunDetails.jsx
@@ -7,85 +7,161 @@ const RunDetails = () => {
     const [run, setRun] = useState({});
     const [location, setLocation] = useState({});
     const [locations, setLocations] = useState([]);
+    const [locationIds, setLocationIds] = useState([]);
     const [place, setPlace] = useState('');
     const [formattedAddress, setFormattedAddress] = useState('');
+    const [editData, setEditData] = useState({
+        distance: '',
+        time: ''
+    });
 
     const apiKey = process.env.REACT_APP_API_KEY;
 
     const getRun = async () => {
         const cachedRun = localStorage.getItem(`run-${id}`);
         if (cachedRun) {
-          setRun(JSON.parse(cachedRun));
+            setRun(JSON.parse(cachedRun));
         } else {
-          const response = await axios.get(`http://localhost:3001/api/runs/run/${id}`);
-          console.log(response.data);
-          setRun(response.data);
-          localStorage.setItem(`run-${id}`, JSON.stringify(response.data));
+            const response = await axios.get(`http://localhost:3001/api/runs/run/${id}`);
+            console.log(response.data);
+            setRun(response.data);
+            localStorage.setItem(`run-${id}`, JSON.stringify(response.data));
         }
-      };
-      
+    };
 
-      const handleAddLocation = async () => {
+
+    const handleAddLocation = async () => {
         const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${place}&key=${apiKey}`);
-        console.log(response.data);
         const { lat, lng } = response.data.results[0].geometry.location;
         setLocation({ lat, lng });
         const formattedAddress = response.data.results[0].formatted_address;
-        await axios.post(`http://localhost:3001/api/locations/${id}`, {
+        const res = await axios.post(`http://localhost:3001/api/locations/${id}`, {
             latitude: lat,
             longitude: lng,
             terrain_type: formattedAddress,
         });
+        const newLocationId = res.data.id;
         setFormattedAddress(formattedAddress);
         setLocations([...locations, formattedAddress]);
+        setLocationIds([...locationIds, newLocationId]);
     };
 
     const getLocations = async () => {
         const response = await axios.get(`http://localhost:3001/api/locations/${id}`);
-        console.log(response.data);
-        setLocations(response.data.filter(location => location.run_id === parseInt(id)).map(location => location.terrain_type));
+        const filteredLocations = response.data.filter(location => location.run_id === parseInt(id));
+        setLocations(filteredLocations.map(location => location.terrain_type));
+        setLocationIds(filteredLocations.map(location => location.id));
+
+    };
+
+    const deleteLocation = async (index) => {
+        const locationId = locationIds[index];
+        await axios.delete(`http://localhost:3001/api/locations/${locationId}`);
+        getLocations();
+    };
+
+    const handleRunEdit = async (e) => {
+        e.preventDefault();
+        await axios.put(`http://localhost:3001/api/runs/${id}`, {
+            distance: editData.distance,
+            time: editData.time
+        });
+
+
+        const updatedRun = { ...run, distance: editData.distance, time: editData.time }
+        setRun(updatedRun)
+        setEditData({
+            distance: '',
+            time: ''
+        });
+
+        localStorage.setItem(`run-${id}`, JSON.stringify({
+            ...run,
+            distance: editData.distance,
+            time: editData.time,
+        }));
+        
     };
     
-      
     
+
+    const handleEditInputChange = (e) => {
+        const { id, value } = e.target;
+        setEditData((prevState) => ({
+            ...prevState,
+            [id]: value
+        }));
+    };
+
+
     useEffect(() => {
         getRun();
         getLocations();
     }, []);
 
+
     return (
         <div className="flex flex-col items-center justify-center mt-20">
-          <h1 className="text-4xl font-bold mb-4">Distance: {run.distance} mi.</h1>
-          <h2 className="text-xl font-semibold mb-8">Time: {run.time}</h2>
-          <div className="flex flex-col items-center justify-center w-full mb-8">
-            <input
-              type="text"
-              value={place}
-              onChange={(e) => setPlace(e.target.value)}
-              placeholder="Enter a location"
-              className="border border-gray-400 rounded-lg py-2 px-4 mb-4 w-full lg:w-1/2"
-            />
-            <button
-              onClick={handleAddLocation}
-              className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300 ease-in-out"
-            >
-              Add Location
-            </button>
-          </div>
-          {locations.length > 0 && (
-            <div className="w-full lg:w-1/2">
-              <h3 className="text-lg font-medium mb-2">Locations:</h3>
-              <ul className="list-disc list-inside">
-                {locations.map((location, index) => (
-                  <li key={index} className="text-gray-700 mb-2">
-                    {location}
-                  </li>
-                ))}
-              </ul>
+            <h1 className="text-4xl font-bold mb-4">Distance: {run.distance} mi.</h1>
+            <h2 className="text-xl font-semibold mb-8">Time: {run.time}</h2>
+            <div className="flex flex-col items-center justify-center w-full mb-8">
+                <input
+                    type="text"
+                    value={place}
+                    onChange={(e) => setPlace(e.target.value)}
+                    placeholder="Enter a location"
+                    className="border border-gray-400 rounded-lg py-2 px-4 mb-4 w-full lg:w-1/2"
+                />
+                <button
+                    onClick={handleAddLocation}
+                    className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300 ease-in-out"
+                >
+                    Add Location
+                </button>
+                <form onSubmit={handleRunEdit} className='flex flex-col items-center my-5'>
+                    <input
+                        className='border-2 rounded-md resize-none w-60'
+                        type="text"
+                        id="distance"
+                        placeholder="Distance"
+                        value={editData.distance}
+                        onChange={handleEditInputChange}
+                        maxLength={255}
+                    />
+                    <input
+                        className='border-2 rounded-md resize-none w-60'
+                        type="text"
+                        id="time"
+                        placeholder="Time"
+                        value={editData.time}
+                        onChange={handleEditInputChange}
+                        maxLength={255}
+                    />
+                    <button type="submit" className="inline-block border-2 rounded-lg bg-white hover:bg-slate-700 hover:text-white text-gray-500 font-semibold py-2 px-4 my-2 transition-all duration-200 ease-in-out transform  hover:scale-110">Edit</button>
+                </form>
+
             </div>
-          )}
+            {locations.length > 0 && (
+                <div className="w-full lg:w-1/2">
+                    <h3 className="text-lg font-medium mb-2">Locations:</h3>
+                    <ul className="list-disc list-inside">
+                        {locations.map((location, index) => (
+                            <li key={index} className="text-gray-700 mb-2">
+                                {location}
+                                <button
+                                    onClick={() => deleteLocation(index)}
+                                    className="ml-10"
+                                >
+                                    X
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </div>
-      );
+    );
+
 };
 
 
