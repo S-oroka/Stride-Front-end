b005180f5a18c5e6548f0a51ae7638c7728d1db2
Jakub <sorok028@umn.edu>
2023-04-04 09:54:54 -0500

    changes

--- src/components/RunDetails.jsx
+++ src/components/RunDetails.jsx
@@ -35,7 +35,7 @@ const RunDetails = () => {
 
 
     const handleAddLocation = async () => {
-        const response = await api.get(`/api/geocode/json?address=${place}&key=${apiKey}`);
+        const response = await api.get(`/api/geocode/json?address=${place}&key=AIzaSyCOXFtbvRuV6GQnTAv0Yex9X8pMzGbhUlI`);
         const { lat, lng } = response.data.results[0].geometry.location;
         setLocation({ lat, lng });
         const formattedAddress = response.data.results[0].formatted_address;
