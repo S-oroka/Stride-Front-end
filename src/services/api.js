fb3d78fdf6e1890ba22aa7edf6ff9a8aa6bbb783
Jakub <sorok028@umn.edu>
2023-04-04 10:18:54 -0500

    bugs

--- src/services/api.js
+++ src/services/api.js
@@ -15,10 +15,6 @@ Client.interceptors.request.use((config) => {
 
 const api = Axios.create({ baseURL: 'https://maps.googleapis.com/maps/' })
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token')
-    if (token) {
-        config.headers['authorization'] = `Bearer ${token}`
-    }
     return config
 },
     (error) => Promise.reject(error)
